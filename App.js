0bf8832d6dfeabcdb3bdd5f312989f999e7d07a5
muna-jemima <jemimamichael13@gmail.com>
2026-06-10 23:50:41 +0100

    boilerplate

--- App.js
+++ App.js
@@ -1,20 +1,93 @@
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+// import { useFonts } from 'expo-font';
+
+// import "./global.css";
+
+
+// export default function App() {
+//   const [fontsLoaded] = useFonts({
+//     'QuickSand-Bold': require('./assets/fonts/Quicksand-Bold.ttf'),
+//     'QuickSand-Medium': require('./assets/fonts/Quicksand-Medium.ttf'),
+//     'QuickSand-Regular': require('./assets/fonts/Quicksand-Regular.ttf'),
+//     'QuickSand-SemiBold': require('./assets/fonts/Quicksand-SemiBold.ttf'),
+//     'QuickSand-Light': require('./assets/fonts/Quicksand-Light.ttf'),
+//   });
+
+//   if (!fontsLoaded) {
+//     return null;
+//   }
+
+//   return (
+//     // <View style={styles.container}>
+//     //   <Text>Open up App.js to start working on your app!</Text>
+//     //   <StatusBar style="auto" />
+//     // </View>
+
+//     <View className="flex-1 items-center justify-center bg-blue-500">
+//       <Text className="text-xl font-bold text-white">
+//         Welcome to Nativewind!
+//       </Text>
+      
+//     </View>
+//   );
+// }
+
+// // const styles = StyleSheet.create({
+// //   container: {
+// //     flex: 1,
+// //     backgroundColor: '#fff',
+// //     alignItems: 'center',
+// //     justifyContent: 'center',
+// //   },
+// // });
+
+
+
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import { Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import { useEffect } from 'react';
+import "./global.css"
+
+SplashScreen.preventAutoHideAsync();
+
+const customFonts = {
+  "Quicksand-Bold": require("./assets/fonts/Quicksand-Bold.ttf"),
+  "Quicksand-Regular": require("./assets/fonts/Quicksand-Regular.ttf"),
+  "Quicksand-Medium": require("./assets/fonts/Quicksand-Medium.ttf"),
+  "Quicksand-SemiBold": require("./assets/fonts/Quicksand-SemiBold.ttf"),
+  "Quicksand-Light": require("./assets/fonts/Quicksand-Light.ttf"),
+};
 
 export default function App() {
+  const [fontsLoaded, error] = useFonts(customFonts);
+
+  useEffect(() => {
+    if (fontsLoaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (error) {
+    throw error;
+  }
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-5xl text-center font-bold text-primary" style={{ fontFamily: "Quicksand-Medium" }} >
+
+        {/* style={{ fontFamily: "Quicksand-Bold" }} */}
+        {/* font-quickSand-bold */}
+        Welcome to Nativewind!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
